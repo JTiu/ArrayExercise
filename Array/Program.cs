d8212e434eb355a95ebe635dabbbf0d4652c520b
James Tiu <jamestiulawyer@gmail.com>
2021-05-19 14:15:23 -0400

    changes to spacing

--- Array/Program.cs
+++ Array/Program.cs
@@ -6,54 +6,54 @@ public class SamplesArray
     {
 
         // Creates and initializes a new integer array and a new Object array.
-        int[] arrayTenInts = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] arrayTenInts = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         Object[] elevenObjectArray = new Object[11] { 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 };
 
         // Prints the initial values of both arrays.
-        Console.WriteLine("Starting Array,");
-        Console.Write("integer array:");
-        PrintValues(arrayTenInts);
+        Console.WriteLine("Starting Array:\n");
+        Console.Write("Integer array:");
+        PrintTheseIntValues(arrayTenInts);
         Console.Write("Object array: ");
-        PrintValues(elevenObjectArray);
+        PrintTheseObjectValues(elevenObjectArray);
 
         // Copies the first thrree elements from the integer array to the Object array.
         System.Array.Copy(arrayTenInts, elevenObjectArray, 3);
 
         // Prints the values of the modified arrays.
-        Console.WriteLine("\nAfter copying the first three elements of the integer array to the Object array,");
+        Console.WriteLine("\nAfter copying the first three elements of the integer array to the Object array:");
         Console.Write("integer array:");
-        PrintValues(arrayTenInts);
+        PrintTheseIntValues(arrayTenInts);
         Console.Write("Object array: ");
-        PrintValues(elevenObjectArray);
+        PrintTheseObjectValues(elevenObjectArray);
 
         // Copies the last element from the Object array to the integer array.
         System.Array.Copy(elevenObjectArray, elevenObjectArray.GetUpperBound(0) - 0, arrayTenInts, arrayTenInts.GetUpperBound(0) - 0, 1);
 
         // Prints the values of the modified arrays.
-        Console.WriteLine("\nAfter copying the last element of the Object array to the integer array,");
+        Console.WriteLine("\nAfter copying the last element of the Object array to the integer array:");
         Console.Write("integer array:");
-        PrintValues(arrayTenInts);
+        PrintTheseIntValues(arrayTenInts);
         Console.Write("Object array: ");
-        PrintValues(elevenObjectArray);
+        PrintTheseObjectValues(elevenObjectArray);
         Console.WriteLine("The End of Main");
         Console.ReadLine();
     }
 
-    public static void PrintValues(Object[] myArr)
+    public static void PrintTheseObjectValues(Object[] stringArray)
     {
-        foreach (Object i in myArr)
+        foreach (Object i in stringArray)
         {
             Console.Write("\t{0}", i);
         }
-        Console.WriteLine();
+        Console.WriteLine("\n^^^Method Prints Object values \n");
     }
 
-    public static void PrintValues(int[] myArr)
+    public static void PrintTheseIntValues(int[] objectArray)
     {
-        foreach (int i in myArr)
+        foreach (int i in objectArray)
         {
             Console.Write("\t{0}", i);
         }
-        Console.WriteLine();
+        Console.WriteLine("\n^^^Method Prints string values \n");
     }
 }
